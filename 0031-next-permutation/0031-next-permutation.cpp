fd2ed85012dc64fb4c445299ec5ec1741e168af3
Renu1418 <140931565+Renu1418@users.noreply.github.com>
2025-09-22 00:17:22 +0530

    Time: 0 ms (100%), Space: 15.7 MB (67.83%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        
-         int n = nums.size();
+        int n = nums.size();
          int i=n-2;
          while(i>=0 && nums[i]>=nums[i+1]){
             i--;
