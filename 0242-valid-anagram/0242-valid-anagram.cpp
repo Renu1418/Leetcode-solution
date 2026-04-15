35b910b4b8720361ea1f18fbc04ed91ec070959c
Renu1418 <140931565+Renu1418@users.noreply.github.com>
2026-04-15 23:32:59 +0530

    Time: 0 ms (100%), Space: 9.9 MB (24.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       
-    unordered_map<char,int>mp;
-      
-      for(char cs:s){
-        mp[cs]++;
-      }
-      for(char ct:t){
-        mp[ct]--;
-      }
+        
+        unordered_map<char,int>mp;
 
-      for(auto c:mp){
-       if(c.second!=0){
-        return false;
-       }
-      }
-     return true;
+        for(char ch:s){
+            mp[ch]++;
+        }
+        for(char c:t){
+            mp[c]--;
+        }
+        for(auto it:mp){
+            if(it.second != 0){
+                return false;
+            }
+        }
+        return true;
     }
 };
