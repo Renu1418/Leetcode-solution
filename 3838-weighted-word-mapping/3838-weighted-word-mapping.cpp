e5733fc2dd82f1fd3973adf390a814c21010ea96
Renu1418 <140931565+Renu1418@users.noreply.github.com>
2026-06-21 09:34:40 +0530

    Time: 0 ms (100%), Space: 43.7 MB (57.8%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
-        unordered_map<char,int>mp;
         
-        for(int i=0;i<26;i++){
-            mp['a'+i] = weights[i];
-        }
          string s = "";
         for(int i=0;i<words.size();i++){
             int sum = 0;
             for(int j=0;j<words[i].size();j++){
-             char ch = words[i][j];
-              sum += mp[ch];
+                char ch = words[i][j];
+              sum += weights[ch-'a'] ;
             }
             int num = sum%26;
             s += ('z' - num);
