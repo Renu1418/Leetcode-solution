ed22fa73010c452bfe280b95183f185ffbf2882e
Renu1418 <140931565+Renu1418@users.noreply.github.com>
2026-07-02 10:55:43 +0530

    Time: 18 ms (7.05%), Space: 23.9 MB (55.62%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -5,12 +5,9 @@ public:
         int idx =0;
         for(int i=0;i<nums.size();i++){
            if(nums[i]!=0){
-            nums[idx] = nums[i];
+            swap(nums[idx],nums[i]);
             idx++;
            }
-        }
-        for(int i=idx;i<nums.size();i++){
-                nums[i]=0;
         }
         
     }
