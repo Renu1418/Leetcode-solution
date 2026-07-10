279bea47e3c536b7ae6ddc094fdcb5f6a19f30d1
Renu1418 <140931565+Renu1418@users.noreply.github.com>
2026-07-10 21:15:59 +0530

    Time: 15 ms (9.49%), Space: 24 MB (7.56%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-       unordered_set<int>st(nums.begin(),nums.end());
-
-       int sum1=0;
-       int sum2=0;
-
-       for(int x:st){
-        sum1 = sum1 +(2*x);
-       }
-
-       for(int y:nums){
-         sum2 =sum2 +y;
-       }
         
-       return sum1-sum2;
+        unordered_map<int,int>mp;
+         int ans =0;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+
+        for(auto it:mp){
+            if(it.second == 1){
+                ans =it.first;
+            }
+        }
+        return ans;
     }
 };
