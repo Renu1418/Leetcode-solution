087c35d5840b51864d3683ecf7eda6f82bfbbec4
Renu1418 <140931565+Renu1418@users.noreply.github.com>
2026-07-12 16:52:00 +0530

    Time: 39 ms (55.31%), Space: 43.1 MB (78.59%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int>ans(arr.begin(),arr.end());
+        
+        sort(ans.begin(),ans.end());
+        unordered_map<int,int>mp;
+        int count =1;
+        for(int i=0;i<ans.size();i++){
+            if(mp.find(ans[i])==mp.end()){
+            mp[ans[i]] =count;
+             count++;
+            }
+        }
+
+        for(int i=0;i<arr.size();i++){
+          ans[i] =mp[arr[i]];
+        }
+
+        return ans;
+    }
+};
