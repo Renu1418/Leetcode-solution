0c4f705c4b9d3f1fc56ea6a197df7f44bce78dd2
Renu1418 <140931565+Renu1418@users.noreply.github.com>
2026-07-14 09:25:03 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.63%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-    }
+        
+        unordered_map<int,int>mp;
+
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        int cnt=0;
+        for(int i=0;i<=2;i++){
+           int num = mp[i];
+           for(int j=0;j<num;j++){
+               
+                nums[cnt] = i;
+                cnt++;
+            }
+           }
+        }
 };
