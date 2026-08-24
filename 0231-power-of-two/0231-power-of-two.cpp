ce50d968a8b16716061aa6d321cd92a490c6670a
Renu1418 <140931565+Renu1418@users.noreply.github.com>
2026-08-24 16:41:14 +0530

    Time: 0 ms (100%), Space: 7.8 MB (82.28%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-
-        if(n <= 0) return false;
         
-        if(n==1){
+        if(n<=0){
+            return false;
+        }
+        if((n&(n-1))==0){
             return true;
         }
-        int
-        long long ans = 1;
-        while(ans<n){
-            ans = ans*2;
-            if(ans==n){
-                return true;
-            }
-        }
-        return false;
+         return false;
     }
 };
