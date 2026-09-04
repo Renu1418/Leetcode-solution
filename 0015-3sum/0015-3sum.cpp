b5222a7e77c650786298326272f4da3c1f1b2729
Renu1418 <140931565+Renu1418@users.noreply.github.com>
2026-09-04 10:29:10 +0530

    Time: 43 ms (82.85%), Space: 29.1 MB (46.21%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,35 +1,48 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
-        for(int i=0;i<nums.size()-2;i++){
-            if(i>0 && nums[i] == nums[i-1]){
-                    continue;
-                }
-            int l=i+1, r=nums.size()-1;
-            while(l<r){
-                
-                int sum = nums[i]+nums[l]+nums[r];
-                if(sum == 0){
-                 ans.push_back({nums[i],nums[l],nums[r]});
-                 l++;
-                 r--;
-                 while(l<r && nums[l] == nums[l-1]){
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size() - 2; i++) {
+
+            // Skip duplicate nums[i]
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int l = i + 1;
+            int r = nums.size() - 1;
+
+            while (l < r) {
+
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[l], nums[r]});
+
                     l++;
-                    }
-                 while(l<r && nums[r] == nums[r+1]){
                     r--;
+
+                    // Skip duplicate left values
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
                     }
-                }  
-                else if(sum<0){
-                    l++;
+
+                    // Skip duplicate right values
+                    while (l < r && nums[r] == nums[r + 1]) {
+                        r--;
+                    }
                 }
-                else{
+                else if (sum > 0) {
                     r--;
+                }
+                else {
+                    l++;
                 }
             }
         }
+
         return ans;
     }
 };
